7236cb5a5acca65264913e79489d1988774800d3
laijingtao <laijingtao@gmail.com>
2018-01-24 23:23:51 -0600

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='paramparser',
+      version='0.1',
+      description='Read parameters from file',
+      author='Jingtao Lai',
+      py_modules=['paramparser']
+     )
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python setup.py install --record files.txt
+cat files.txt | xargs sudo rm -rf
+rm files.txt
